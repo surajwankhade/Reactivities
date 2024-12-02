cd326f38d67d0eb820cd696cc3a537a9367895d3
Suraj <surajwankhade56@gmail.com>
2024-12-02 10:52:40 -0600

    End of section 9 2

--- client-app/src/features/activities/dashboard/ActivityFilters.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -3,8 +3,8 @@ import Calendar from "react-calendar";
 export default function ActivityFilters() {
     return (
         <>
-            <Menu clearing vertical size='large' style={{ width: '100%', marginTop: 25}}>
-                <Header icon='filter' attched color='teal' content='Filters' />
+            <Menu vertical size='large' style={{ width: '100%', marginTop: 25}}>
+                <Header icon='filter' attached color='teal' content='Filters' />
                 <Menu.Item content='All Activities' />
                 <Menu.Item content="I'm going" />
                 <Menu.Item content="I'm hosting" />
