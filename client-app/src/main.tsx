aaba6296f20dfad22274c325013f9e1d5e28cd1a
Suraj <surajwankhade56@gmail.com>
2024-12-02 10:48:23 -0600

    End of section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { store, StoreContext } from './app/stores/store'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes'
-
+import 'react-calendar/dist/Calendar.css'
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <StoreContext.Provider value={store}>
